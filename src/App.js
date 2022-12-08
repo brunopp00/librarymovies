f7efea82dc68902cc6aa4840161b072218306f2e
bruno <brunof@unimedvaledocai.com.br>
2022-12-08 13:43:34 -0300

    App 'pronto'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+import { AppBar, Box, Card, CardContent, CardMedia, Dialog, DialogContent, DialogTitle, Grid, Toolbar, Typography } from '@mui/material';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+    const [movies, setMovies] = useState([])
+    const [openModal, setOpenModal] = useState(false)
+    const [move, setMove] = useState({})
+
+    const getMovies = useCallback(() => {
+
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=0f7f734413859ad69f2aee2b915bed16')
+        .then(res => res.json())
+        .then(data => {
+            setMovies(data.results)
+        })
+    },[])
+
+    useEffect(() => {
+        getMovies()
+    }, [getMovies])
+    console.log(movies)
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={1}>
+      <AppBar position='static' sx={{backgroundColor: 'black'}}>
+        <Toolbar>
+            <Box display='flex' justifyContent='center' width='100vw'>
+                <h1 className='title'>THE MOVIES</h1>
+            </Box>
+        </Toolbar>
+      </AppBar>
+      {movies.map(move => 
+        <Grid item xs={6} lg={2}>
+            <Card onClick={() => {
+                setOpenModal(true)
+                setMove(move)
+                }} key={move.title} className='cardMove'>
+                <CardMedia
+                    component='img'
+                    height='350'
+                    image={`https://image.tmdb.org/t/p/w500/${move.poster_path}`}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant='h6' align='center' component='div'>
+                            {move.title}
+                        </Typography>
+                    </CardContent>
+            </Card>
+        </Grid>
+        )}
+        <Dialog open={openModal} onClose={() => setOpenModal(false)} maxWidth='lg'fullWidth>
+            <DialogTitle><Typography variant='h4'>{move.title}</Typography></DialogTitle>
+            <DialogContent>
+                <Grid container spacing={1}>
+                    <Grid item xs={6}>
+                        <Typography variant='h5' style={{marginBottom:'20px'}}>
+                            {move.overview}
+                        </Typography>
+                        <Typography variant='body2'>
+                            Release date of: {move.release_date}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <Box
+                        component='img'
+                        height='300px'
+                        src={`https://image.tmdb.org/t/p/w500/${move.backdrop_path}`}
+                        />
+                    </Grid>
+                </Grid>
+            </DialogContent>
+        </Dialog>
+    </Grid>
   );
 }
 
